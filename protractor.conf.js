e6bcc73054b2fe80670a0875662e1e2c091b9edd
Xander Arling <xander.arling@gmail.com>
2015-03-09 17:47:09 +0100

    Voeg javascript voorbeelden toe

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,28 +1,34 @@
 exports.config = {
-  // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+	// The address of a running selenium server.
+	seleniumAddress: 'http://localhost:4444/wd/hub',
 
-  // Spec patterns are relative to the location of this config.
-  specs: [
-    'spec/*_spec.js'
-  ],
-
-
-  capabilities: {
-    'browserName': 'firefox',
-    'chromeOptions': {'args': ['--disable-extensions']}
-  },
+	// Spec patterns are relative to the location of this config.
+	specs: [
+		'spec/*_spec.js'
+	],
 
 
-  // A base URL for your application under test. Calls to protractor.get()
-  // with relative paths will be prepended with this.
-  baseUrl: 'http://www.wehkamp.nl',
 
-  jasmineNodeOpts: {
-    onComplete: null,
-    isVerbose: false,
-    showColors: true,
-    includeStackTrace: true,
-    defaultTimeoutInterval: 10000
-  }
+
+	capabilities: {
+		'browserName': 'firefox',
+		'chromeOptions': {'args': ['--disable-extensions']}
+	},
+
+	onPrepare: function () {
+		browser.ignoreSynchronization = true;
+	},
+
+
+	// A base URL for your application under test. Calls to protractor.get()
+	// with relative paths will be prepended with this.
+	baseUrl: 'http://www.wehkamp.nl',
+
+	jasmineNodeOpts: {
+		onComplete: null,
+		isVerbose: false,
+		showColors: true,
+		includeStackTrace: true,
+		defaultTimeoutInterval: 10000
+	}
 };
